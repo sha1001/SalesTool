31b9b5f0dbb0c43d06f32e89940808bd7b9ebd19
Other Developer <junk.coder.email@gmail.com>
2013-05-21 19:53:37 -0700

    Summed up values when displaying and added class comment.

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -1,13 +1,20 @@
 package ca.demo.salestool;
 
+/**
+ * Represent the sales data for a given period.
+ */
 public class SalesData {
 	int data[] = {0, 4, 42};
 	
 	public void display() {
 		System.out.println("Data: ");
+		int sum = 0;
 		for(int i = 0; i < data.length; i++) {
 			System.out.println("Next value: " + data[i]);
+			sum += i;
 		}
+		
+		System.out.println("Sum is: " + sum);
 	}
 
 }
